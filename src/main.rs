f8209a6395e3a9a8feffa1494336310b31c05bb6
Christian Staib <staib.christian@hotmail.de>
2023-06-06 22:20:31 +0200

    improved image generation

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@ use std::time::Instant;
 
 #[derive(Clone, Copy)]
 struct Point {
-    x: f32,
-    y: f32,
+    x: f64,
+    y: f64,
 }
 
 struct CliffordAttractor {
     point: Point,
-    a: f32,
-    b: f32,
-    c: f32,
-    d: f32,
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
 }
 
-fn scale(a_min: f32, a_max: f32, b_min: u32, b_max: u32, x: f32) -> u32 {
+fn scale(a_min: f64, a_max: f64, b_min: u32, b_max: u32, x: f64) -> u32 {
     let x_in_percent = (x - a_min) / (a_max - a_min);
-    b_min + (((b_max - b_min) as f32) * x_in_percent) as u32
+    b_min + (((b_max - b_min) as f64) * x_in_percent) as u32
 }
 
 fn is_between(min: u32, max: u32, x: u32) -> bool {
@@ -48,10 +48,10 @@ fn main() {
         d: -1.2,
     };
 
-    let iterations = 50_000_000_000;
+    let iterations = 5_000_000_000;
 
-    let img_rows = 2000;
-    let img_columns = 2000;
+    let img_rows = 4000;
+    let img_columns = 4000;
     let x_min = -2.5;
     let x_max = 2.5;
     let y_min = -2.5;
@@ -70,26 +70,48 @@ fn main() {
             pixels[(scaled_x + scaled_y * img_columns as u32) as usize] += 1;
         }
 
+        // updating every iteration is too slow as IO becomes bottleneck
         if i % update_intervall == 0 {
             progress_bar.inc(update_intervall as u64);
         }
     }
     progress_bar.finish();
 
-    let max_pixel_value = pixels.iter().max().unwrap();
+    // take log
+
     pixels = pixels
         .iter()
-        .map(|x| (*x as f32) as u64)
-        //.map(|x| ((*x as f32) / (*max_pixel_value as f32) * (u16::MAX as f32)) as u64)
+        .map(|x| (*x as f32).log(1.01) as u64)
         .collect();
+
+    // scale median_pixel_value to factor*u16::MAX
+    let mut pixels_copy = pixels.clone();
+    pixels_copy.sort_unstable();
+
+    pixels_copy = pixels_copy
+        .into_iter()
+        .collect::<std::collections::HashSet<_>>()
+        .into_iter()
+        .collect();
+
+    let max_pixel = pixels
+        .iter()
+        .map(|x| {
+            pixels_copy
+                .iter()
+                .position(|y| x == y)
+                .expect("value not found")
+        })
+        .max()
+        .expect("list empty");
 
     let gray_image: ImageBuffer<Luma<u16>, Vec<u16>> = ImageBuffer::from_raw(
         img_columns as u32,
         img_rows as u32,
         pixels
             .iter()
-            .map(|x| *x as u16)
-            //.map(|x| (std::cmp::min(u16::MAX as u64, *x) as u16))
+            .map(|x| ((*x as f32 / max_pixel as f32) * u16::MAX as f32) as u64)
+            .map(|x| u16::MAX - (std::cmp::min(u16::MAX as u64, x) as u16))
             .collect(),
     )
     .unwrap();
